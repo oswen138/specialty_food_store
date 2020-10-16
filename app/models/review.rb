96a0667789e942dd35c08f5579e0770085860218
ophelia swen <ophelia.swen@gmail.com>
2020-10-16 16:12:42 -0700

    not sure how to fix these test errors...trying different solutions

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,6 @@
 class Review < ApplicationRecord
+  validates_numericality_of :rating, greater_than_or_equal_to: 1
+  validates_length_of :content_body, minimum: 50
   validates_length_of :content_body, maximum: 250
   belongs_to :product
-end
+endtrl;fjsl;ejgl;kseg
