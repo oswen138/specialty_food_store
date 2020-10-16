1fac998fc42fb47a3d2c19e20666585919b18763
ophelia swen <ophelia.swen@gmail.com>
2020-10-16 12:13:33 -0700

    follow lesson 18 and add titlelize method and test for it

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,7 +1,55 @@
-class SongsController < ApplicationController
+class ReviewsController < ApplicationController
+
+  before_action :authorize, only: [:new]
   def new
-    @album = Album.find(params[:album_id])
-    @song = @album.songs.new
-    render :new
+    @product = Product.find(params[:product_id])
+    @review = @product.reviews.new
+  end
+
+  def create
+    @product = Product.find(params[:product_id])
+    @review = @product.reviews.new(review_params)
+    if @review.save
+      flash[:notice] = "Review successfully added!"
+      redirect_to product_path(@product)
+    else
+      render :new
+    end
+  end
+
+  def index
+    @products = Product.all
+  end
+
+  def show
+    @product = Product.find(params[:id])
+    @review = Review.find(params[:id])
+  end
+
+  def edit
+    @product = Product.find(params[:product_id])
+    @review = Review.find(params[:id])
+  end
+
+  def update
+    @product = Product.find(params[:product_id])
+    @review = Review.find(params[:id])
+    if @review.update(review_params)
+      redirect_to product_path(@product)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @product = Product.find(params[:product_id])
+    @review = Review.find(params[:id])
+    @review.destroy
+    redirect_to product_path(@product)
+  end
+
+  private
+  def review_params
+    params.require(:review).permit(:author, :content_body, :rating)
   end
 end
--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -3,3 +3,10 @@ require 'rails_helper'
 describe Product do
   it { should have_many(:reviews) }
 end
+
+describe Product do
+  it("titleizes the name of an product") do
+    product = Product.create({name: "broccoli", country_of_origin: "italy"})
+    expect(product.name()).to(eq("Broccoli"))
+  end
+end
