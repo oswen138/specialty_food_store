332febe0a1575f9b044fb287c84bb6fd11eec6c1
ophelia swen <ophelia.swen@gmail.com>
2020-10-23 11:24:59 -0700

    figuring out how to add admin authorization

--- db/schema.rb
+++ db/schema.rb
@@ -28,6 +28,8 @@ ActiveRecord::Schema.define(version: 2020_10_23_162820) do
     t.string "content_body"
     t.integer "rating"
     t.integer "product_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "users", force: :cascade do |t|
