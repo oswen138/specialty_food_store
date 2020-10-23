52fe58ab037df4c03075731ee353edefafad731a
ophelia swen <ophelia.swen@gmail.com>
2020-10-23 13:34:38 -0700

    trying to set up admin auth still...a bit confused but still trying

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_23_162820) do
+ActiveRecord::Schema.define(version: 2020_10_23_202457) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,8 +28,6 @@ ActiveRecord::Schema.define(version: 2020_10_23_162820) do
     t.string "content_body"
     t.integer "rating"
     t.integer "product_id"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "users", force: :cascade do |t|
@@ -40,6 +38,7 @@ ActiveRecord::Schema.define(version: 2020_10_23_162820) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.boolean "admin", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
