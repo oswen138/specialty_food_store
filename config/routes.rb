1d2b75df59b7f6c9d7a822f8844299170af65452
ophelia swen <ophelia.swen@gmail.com>
2020-10-23 14:50:14 -0700

    uninitialized constant error prohbiting progress..searching for bug

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :users, only: [:index, :show, :edit, :update]
-  # get 'home/index'
-  
-  resources :reviews
   root to: 'home#index'
+ 
+
 end
 #   root to: 'products#index'
 #   resources :products do
